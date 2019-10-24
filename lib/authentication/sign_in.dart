ac9a5314add3517391fdc27747664e6b317af300
Shilpa Sethi <shilpa.sethi@sky.uk>
2019-10-24 18:02:25 +0100

    added logout for google sign-in

--- lib/authentication/sign_in.dart
+++ lib/authentication/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:Timeline/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -28,6 +29,5 @@ Future<String> signInWithGoogle() async {
 
 void signOutGoogle() async {
   await googleSignIn.signOut();
-
   print("User Sign Out");
 }
--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:Timeline/authentication/sign_in.dart';
 
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.blue[100]),
+      appBar: AppBar(
+        title: const Text('Welcome to timeline'),
+        actions: <Widget>[
+          signoutButton()
+        ],
+
+      ),
+      body: Container(color: Colors.white12),
+    );
+  }
+
+  Widget signoutButton() {
+    return IconButton(
+      icon: Icon(Icons.exit_to_app),
+      tooltip: "Logout",
+      splashColor: Colors.yellow,
+      onPressed: () {
+        signOutGoogle();
+      },
     );
   }
 }
